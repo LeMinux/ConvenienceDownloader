5267a6913027d2925d4565b23d15c6c1e85a0916
LeMinux <MatthewLoz@protonmail.com>
2024-05-29 19:05:02 -0400

    Removed comment blocks

--- helpers.c
+++ helpers.c
@@ -7,31 +7,6 @@ void printError(int code, const char* message){
 	exit(code);
 }
 
-/*
-//helper method to get input and clears stdin
-//also nul terminates the string
-void exactUserInput(char* input, int buffer){
-	int index = 0;
-	int data = 0;
-	switch(buffer){
-		case 1:
-			*input = getchar();
-		break;
-		default:
-			while(index < buffer - 1 && (data = getchar()) != '\n' && data != EOF)
-				*(input + index++) = data;
-
-			*(input + index) = '\0';
-		break;
-	}
-
-	//clears stdin
-	if(data != '\n')
-		while(getchar() != '\n'){}
-
-}
-*/
-
 //surrounds a string in quotes
 //simplifies needing to do this\ is\ a\ test
 //single quotes could be used, but they can not escape characters
@@ -69,28 +44,3 @@ char* surroundInQuotes(const char* surround){
 	newString[length] = '\0';
 	return newString;
 }
-
-/*
-void grepIntoFile(const char* text){
-	const char GREP_PT1 [] = "ls | grep \"";
-	const char GREP_PT2 [] = "\" > GrepTemp.txt";
-	
-	//greps text so that it can be analyized more
-	char* grepCommand = NULL;
-	if(text[0] == '-'){
-		int length = strlen(GREP_PT1) + strlen(text) + strlen(GREP_PT2) + 1;
-		grepCommand = malloc(length + 1);
-		if(grepCommand == NULL) printError(EXIT_FAILURE, FAILED_MALLOC_MSG);
-		snprintf(grepCommand, length + 1, "%s\\%s%s", GREP_PT1, text, GREP_PT2);
-	}else{
-		int length = strlen(GREP_PT1) + strlen(text) + strlen(GREP_PT2);
-		grepCommand = malloc(length + 1);
-		if(grepCommand == NULL) printError(EXIT_FAILURE, FAILED_MALLOC_MSG);
-		snprintf(grepCommand, length + 1, "%s%s%s", GREP_PT1, text, GREP_PT2);
-	}
-
-	printf(PNT_GREEN "%s\n" PNT_RESET, grepCommand);
-	if(system(grepCommand) != 0) printError(EXIT_FAILURE, FAILED_GREP_MSG);
-	free(grepCommand);
-}
-*/
