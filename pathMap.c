7ff5f51333d5eac79b9814b83228ec60f15a123a
LeMinux <MatthewLoz@protonmail.com>
2024-06-11 21:15:25 -0400

    getting subdirectories for array implemention works

--- pathMap.c
+++ pathMap.c
@@ -8,62 +8,11 @@ static void getSubdirectories(Map_t* map, const char* basePath){
 	struct dirent* direntp = NULL;
 	DIR* dirp = NULL;
 	size_t pathLength = 0;
-
-	//This determines if a slash is needed and fits
-	//it is done here so that it doesn't create the large array
-	//just for it to fail existing as a directory if that happens
 	pathLength = strlen(basePath);
-	if(pathLength == 0) return;
+	if(pathLength == 0 || pathLength > _POSIX_PATH_MAX) return;
 
-	int catMode = -1;
-	if(basePath[pathLength - 1] != '/'){
-		if(pathLength + 2 > _POSIX_PATH_MAX) return;
-		++pathLength;
-		catMode = ADD_SLASH;
-	}else{
-		if(pathLength + 1 > _POSIX_PATH_MAX) return;
-		catMode = NO_SLASH;
-	}
-
-	//opens directory
 	dirp = opendir(basePath);
 	if(dirp == NULL) return;
-	//Passes checks and can be added to map
-
-	//POSIX_PATH_MAX contains null count in it
-	char fullPath [_POSIX_PATH_MAX] = "";
-	switch(catMode){
-		case NO_SLASH:
-				strcpy(fullPath, basePath);
-		break;
-
-		case ADD_SLASH:
-				strcpy(fullPath, basePath);
-				strcat(fullPath, "/");
-		break;
-
-		default:
-				PRINT_ERROR("Program encountered courruption during execution");
-				exit(EXIT_FAILURE);
-		break;
-	}
-
-	++map->length;
-	//increase size of header to point to another string
-	if((map->map = realloc(map->map, sizeof(map->map) * map->length)) == NULL){
-		PRINT_ERROR(FAILED_MALLOC_MSG);
-		exit(EXIT_FAILURE);
-	}
-
-	//allocate space for string
-	map->map[map->length - 1] = malloc(pathLength + 1);
-
-	if(map->map[map->length - 1] == NULL){
-		PRINT_ERROR(FAILED_MALLOC_MSG);
-		exit(EXIT_FAILURE);
-	}
-	strcpy(map->map[map->length - 1], fullPath);
-	map->map[map->length - 1][pathLength - 1] = '\0';
 
 	while((direntp = readdir(dirp)) != NULL){
 		/* Ignore special directories. */
@@ -71,37 +20,73 @@ static void getSubdirectories(Map_t* map, const char* basePath){
 			(strcmp(direntp->d_name, "..") == 0))
 			continue;
 
-		/* Calculate full name, check we are in file length limts */
-		//+1 for nul byte
-		if((pathLength + strlen(direntp->d_name) + 1) > _POSIX_PATH_MAX){
-			fprintf(stderr, "File path exceeds %d: %s%s\n", _POSIX_PATH_MAX, basePath, direntp->d_name);
-			continue;
+		char fullPath [_POSIX_PATH_MAX] = "";
+		size_t fullLength = pathLength + strlen(direntp->d_name);
+
+		//Calculate full name, check we are in file length limts
+		if(basePath[pathLength - 1] != '/'){
+			//+1 for nul byte +1 for '/'
+			if(fullLength + 2 > _POSIX_PATH_MAX){
+				(void)fprintf(stderr, "File path exceeds %d: %s/%s\n", _POSIX_PATH_MAX, basePath, direntp->d_name);
+				continue;
+			}
+			(void)strcpy(fullPath, basePath);
+			(void)strcat(fullPath, "/");
+			++fullLength;
 		}else{
-			strcat(fullPath, direntp->d_name);
+			//+1 for nul byte
+			if(fullLength + 1 > _POSIX_PATH_MAX){
+				(void)fprintf(stderr, "File path exceeds %d: %s%s\n", _POSIX_PATH_MAX, basePath, direntp->d_name);
+				continue;
+			}
+			(void)strcpy(fullPath, basePath);
 		}
+		(void)strcat(fullPath, direntp->d_name);
+		fullPath[fullLength] = '\0';
 
 		struct stat fstat = {0};
-		if(stat(fullPath, &fstat) < 0){
+		if(stat(fullPath, &fstat) < 0)
 			continue;
-			memset(fullPath + pathLength + 1, '\0', _POSIX_PATH_MAX - pathLength - 1);
-		}
 
 		if(S_ISDIR(fstat.st_mode))
 			getSubdirectories(map, fullPath);
+	}//end while
 
+	//this does make the ordering reversed, but getting directories is
+	//not in order anyway
+
+	//increase size of header to point to another string
+	map->map = realloc(map->map, sizeof(*map->map) * (map->length + 1));
+	if(map->map == NULL){
+		PRINT_ERROR(FAILED_MALLOC_MSG);
+		exit(EXIT_FAILURE);
 	}
-	closedir(dirp);
+
+	//allocate space for string
+	map->map[map->length] = malloc(pathLength + 1);
+
+	if(map->map[map->length] == NULL){
+		PRINT_ERROR(FAILED_MALLOC_MSG);
+		exit(EXIT_FAILURE);
+	}
+	(void)strcpy(map->map[map->length], basePath);
+	map->map[map->length][pathLength] = '\0';
+	++map->length;
+	(void)closedir(dirp);
+}
+
+static void sortMap(Map_t map){
+
 }
 
 Map_t* obtainPathMap(const char* initialDirPath){
 	if(initialDirPath == NULL) return NULL;
 
-	Map_t* retMap = malloc(sizeof(*retMap));
+	Map_t* retMap = calloc(1, sizeof(*retMap));
 	if(retMap == NULL){
 		PRINT_ERROR(FAILED_MALLOC_MSG);
 		exit(EXIT_FAILURE);
 	}
-	retMap->length = 0;
 	getSubdirectories(retMap, initialDirPath);
 	return retMap;
 }
@@ -118,6 +103,5 @@ void freePathMap(Map_t* pathMap){
 
 void printPathMap(Map_t* pathMap){
 	int p = 0;
-	for(; p < pathMap->length; ++p) printf("%d> %s", p + 1, pathMap->map[p]);
-	free(pathMap);
+	for(; p < pathMap->length; ++p) printf("%d> %s\n", p + 1, pathMap->map[p]);
 }
