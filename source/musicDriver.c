9e964ebe745af90387f032725bb91c29303b3a09
LeMinux <MatthewLoz@protonmail.com>
2024-06-27 20:15:29 -0400

    Cover destination is accounted for with swap tags

--- source/musicDriver.c
+++ source/musicDriver.c
@@ -585,7 +585,7 @@ int main(int argc, char** argv){
 							offset = 2;
 						break;
 						case 'c': case 'C':
-							findPath(&destMaps[COVER_INDEX], buffer + 3);
+							newDest = findPath(&destMaps[COVER_INDEX], buffer + 3);
 							offset = 3;
 						break;
 						default:
