a2292ffd79c79182b4c05e1197cfe9ab75871389
LeMinux <>
2025-12-14 18:30:20 -0500

    Beginning testing

---
+++ Test/main_unit_test.c
@@ -0,0 +1,23 @@
+/*
+*
+* This is the unit test file for main's arguments.
+* The functions that main uses will be in their own unit test
+*
+*/
+
+#define SUCCESS 1
+#define FAILURE 0
+
+void testListOption();
+void testDeepListOption();
+void testFileOption();
+void testCovertOption();
+void testWriteAudioOption();
+void testWriteVideoOption();
+void testHelpOption();
+void testVersionOption();
+
+int main(void){
+
+    
+}
