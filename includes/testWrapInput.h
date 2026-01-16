6083beb6cde34ceaa5d864d3fcde80c4815af8f8
LeMinux <>
2026-01-15 20:50:35 -0500

    Added some more __wrap_ functions for stubbing

--- includes/testWrapInput.h
+++ includes/testWrapInput.h
@@ -2,8 +2,13 @@
 #define TEST_WRAP_INPUT_H
 
 #include <string.h>
+#include <stdlib.h>
+
 #include "./testGlobals.h"
 
 int __wrap_boundedInput(FILE* stream, char* dest, size_t dest_size);
+int __wrap_takeIndexInput(int max);
+int __wrap_takeDepthInput(void);
+char* __wrap_takeDirectoryInput(void);
 
 #endif
--- source/testWrapInput.c
+++ source/testWrapInput.c
@@ -5,9 +5,46 @@ int __wrap_boundedInput(FILE* stream, char* dest, size_t dest_size){
     (void)stream;
     (void)dest_size;
 
-    char* set_string = mock_type(char*);
+    const char* stubbed_string = mock_type(char*);
     int write_len  = mock_type(int);
-    memcpy(dest, set_string, write_len);
+    memcpy(dest, stubbed_string, write_len);
     dest[write_len] = '\0';
     return write_len;
+}
+
+int __wrap_takeIndexInput(int max){
+    (void) max;
+    function_called();
+    if(has_mock()){
+        return mock_type(int);
+    }else{
+        stop();
+    }
+}
+
+//malloc is used as the path returned is expected to be malloced
+char* __wrap_takeDirectoryInput(void){
+    function_called();
+    if(has_mock()){
+        char* ret_string = NULL;
+        const char* stubbed_string = mock_type(char*);
+        if(stubbed_string != NULL){
+            int stubbed_size = strlen(stubbed_string) + 1;
+            ret_string = malloc(stubbed_size);
+            memcpy(ret_string, stubbed_string, stubbed_size);
+        }
+
+        return ret_string;
+    }else{
+        stop();
+    }
+}
+
+int __wrap_takeDepthInput(){
+    function_called();
+    if(has_mock()){
+        return mock_type(int);
+    }else{
+        stop();
+    }
 }
