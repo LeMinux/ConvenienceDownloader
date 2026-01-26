f157ece55933d05d3bd9d81cff0b6be77390ebd7
LeMinux <>
2026-01-25 19:22:58 -0500

    Changed wrapped input to have an extern and compile when not wrapping others

--- includes/testWrapInput.h
+++ includes/testWrapInput.h
@@ -6,6 +6,10 @@
 
 #include "./testGlobals.h"
 
+enum CALL{NO_CALL = 0, CALL};
+
+extern enum CALL call_real_function;
+
 int __wrap_boundedInput(FILE* stream, char* dest, size_t dest_size);
 int __wrap_takeIndexInput(int max);
 int __wrap_takeDepthInput(void);
--- source/testWrapInput.c
+++ source/testWrapInput.c
@@ -1,9 +1,19 @@
 #include "../includes/testWrapInput.h"
 
+enum CALL call_real_function = NO_CALL;
+
+//really here just so the compiler doesn't say undefiend references to the __real_
+//functions that aren't wrapped
+//If there is no corresponding __real_ then that function hasn't been wrapped
+//, and neither __wrap_ nor __real_ will be called because they don't exist.
+int __real_takeDepthInput(void) __attribute__((weak));
+int __real_takeIndexInput(int) __attribute__((weak));
+char* __real_takeDirectoryInput(void) __attribute__((weak));
+
 int __wrap_boundedInput(FILE* stream, char* dest, size_t dest_size){
     //don't care about these just want to return a string
-    (void)stream;
-    (void)dest_size;
+    (void) stream;
+    (void) dest_size;
 
     const char* stubbed_string = mock_type(char*);
     int write_len  = mock_type(int);
@@ -12,39 +22,64 @@ int __wrap_boundedInput(FILE* stream, char* dest, size_t dest_size){
     return write_len;
 }
 
+//If a mock is available it will return the mock first.
+//the usage of __real_ function is there to determine behavior once the mock
+//is consumed or if there is no mock to be had.
+//This may be wanted because the below functions also use boundedInput, so if
+//the actual behavior of these functions are wanted it can be tested.
+
 int __wrap_takeIndexInput(int max){
-    (void) max;
     function_called();
+    int ret_int = 0;
+
     if(has_mock()){
-        return mock_type(int);
+        ret_int = mock_type(int);
     }else{
-        stop();
+        if(call_real_function == CALL){
+            ret_int = __real_takeIndexInput(max);
+        }else{
+            stop();
+        }
     }
+    return ret_int;
 }
 
 //malloc is used as the path returned is expected to be malloced
 char* __wrap_takeDirectoryInput(void){
     function_called();
+    char* ret_string = NULL;
+
     if(has_mock()){
-        char* ret_string = NULL;
         const char* stubbed_string = mock_type(char*);
         if(stubbed_string != NULL){
             int stubbed_size = strlen(stubbed_string) + 1;
             ret_string = malloc(stubbed_size);
             memcpy(ret_string, stubbed_string, stubbed_size);
         }
-
-        return ret_string;
     }else{
-        stop();
+        if(call_real_function == CALL){
+            ret_string = __real_takeDirectoryInput();
+        }else{
+            stop();
+        }
     }
+
+    return ret_string;
 }
 
-int __wrap_takeDepthInput(){
+int __wrap_takeDepthInput(void){
     function_called();
+    int ret_int = 0;
+
     if(has_mock()){
-        return mock_type(int);
+        ret_int = mock_type(int);
     }else{
-        stop();
+        if(call_real_function == CALL){
+            ret_int = __real_takeDepthInput();
+        }else{
+            stop();
+        }
     }
+
+    return ret_int;
 }
