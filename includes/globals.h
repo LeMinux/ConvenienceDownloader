3b71dbc3a37220b4e6cc782176a7dad6ca7eb982
LeMinux <>
2026-02-05 22:51:50 -0500

    Added a SKIPPING enum value

--- includes/globals.h
+++ includes/globals.h
@@ -11,7 +11,6 @@
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
 #define CYAN    "\033[36m"
-#define YOUTUBE_URL "https://www.youtube.com/watch?v="
 
 #define BUFFER_SIZE 101
 #define LARGER_BUFFER_SIZE 201
@@ -19,8 +18,8 @@
 #define INF_DEPTH -5
 
 enum ERROR {HAD_ERROR = -1, NO_ERROR = 0};
-enum CONFIG {AUDIO_CONFIG = 1, VIDEO_CONFIG, COVER_CONFIG, BLACK_CONFIG};
-enum INPUT {INVALID = -1, VALID};
+enum CONFIG {NOT_A_CONFIG = -1, AUDIO_CONFIG = 1, VIDEO_CONFIG, COVER_CONFIG, BLACK_CONFIG};
+enum INPUT {SKIPPING = -2, INVALID, VALID};
 
 #define PRINT_ERROR(message_) ((void)fputs(RED message_ RESET"\n", stderr))
 
@@ -31,11 +30,5 @@ enum INPUT {INVALID = -1, VALID};
 #define ADVISE_USER(message_) ((void)puts(CYAN message_ RESET))
 
 #define ADVISE_USER_FORMAT(format_, ...) ((void)fprintf(stderr, CYAN format_ RESET"\n", __VA_ARGS__))
-
-//general failed malloc message
-#define FAILED_MALLOC_MSG "\nError MEM: Allocation of memory failed"
-
-#define YT_URL_INPUT_SIZE 44 //this is from the beginning of the URL to the end of the ID (32 + 11 + 1)
-#define YT_ID_SIZE 12
 
 #endif
