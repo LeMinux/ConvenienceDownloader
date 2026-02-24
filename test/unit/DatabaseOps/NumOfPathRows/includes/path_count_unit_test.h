c4f6579ff4339e55f72b45faee58f0f3532c3bf8
LeMinux <>
2026-02-23 19:04:42 -0500

    Added a unit test for database path counts

---
+++ test/unit/DatabaseOps/NumOfPathRows/includes/path_count_unit_test.h
@@ -0,0 +1,11 @@
+#ifndef PATH_COUNT_TEST_H
+#define PATH_COUNT_TEST_H
+
+#include "testGlobals.h"
+#include "testSetups.h"
+#include "databaseOps.h"
+
+void testGetNumOfPathRowsGivesZero(void** state);
+void testGetNumOfPathRowsCorrectCount(void** state);
+
+#endif
